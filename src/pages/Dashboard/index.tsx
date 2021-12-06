30ac70669ae3132858887e8032917d8995914124
Lucas Prado <lukep.onbass@gmail.com>
2021-12-06 16:20:42 -0300

    Continuing Occurrences Dashboard's development

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -8,7 +8,19 @@ export function Dashboard() {
             <Header />
 
             <div className={styles.contentWrapper}>
+
                 <h1 className={styles.pageTitle}>Dashboard de ocorrências</h1>
+                <div className={styles.break}></div>
+                <div className={styles.chartsContainer}>
+                    <div className={styles.chartContainer}>
+                        <h2 className={styles.chartTitle}>Gráfico 1</h2>
+                        <p>Aqui virá o gráfico 1.</p>
+                    </div>
+                    <div className={styles.chartContainer}>
+                        <h2 className={styles.chartTitle}>Gráfico 2</h2>
+                        <p>Aqui virá o gráfico 2</p>
+                    </div>
+                </div>
             </div>
 
             <Footer />
